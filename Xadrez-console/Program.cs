32a1055f14362a3499e8ce1c5e0b3480e9a312cf
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-02-13 23:20:39 -0300

    Testes iniciais dos movimentos das pecas

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,10 +8,32 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            try {
+            try
+            {
 
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);  
+                }
+
 
                 Tela.imprimirTabuleiro(partida.tab);
             }
@@ -20,7 +42,7 @@ namespace Xadrez_console
                 Console.WriteLine(e.Message);
             }
             Console.ReadLine();
-            
+
         }
     }
 }
